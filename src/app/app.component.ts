7b113d6e8b66481642eca1d3b2120c2292c01ac8
gerasimidisk <gerasimidisk00@gmail.com>
2024-12-27 03:48:35 +0200

    login user finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { ListGroupMenuComponent } from './components/list-group-menu/list-group-menu.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 
 
@@ -9,7 +10,7 @@ import { ListGroupMenuComponent } from './components/list-group-menu/list-group-
   standalone: true,
   imports: [RouterLink,
     RouterOutlet,
-    ListGroupMenuComponent],
+    ListGroupMenuComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
