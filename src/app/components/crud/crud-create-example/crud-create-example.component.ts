9cf635da0bf5e135f68dccfbd99b6f4dc02730a7
gerasimidisk <gerasimidisk00@gmail.com>
2024-12-30 03:34:46 +0200

    crud scaffolding

---
+++ src/app/components/crud/crud-create-example/crud-create-example.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CrudNavbarComponent } from '../crud-navbar/crud-navbar.component';
+
+@Component({
+  selector: 'app-crud-create-example',
+  imports: [CrudNavbarComponent],
+  templateUrl: './crud-create-example.component.html',
+  styleUrl: './crud-create-example.component.css'
+})
+export class CrudCreateExampleComponent {
+
+}
---
+++ src/app/components/crud/crud-read-example/crud-read-example.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CrudNavbarComponent } from '../crud-navbar/crud-navbar.component';
+
+@Component({
+  selector: 'app-crud-read-example',
+  imports: [CrudNavbarComponent],
+  templateUrl: './crud-read-example.component.html',
+  styleUrl: './crud-read-example.component.css'
+})
+export class CrudReadExampleComponent {
+
+}
